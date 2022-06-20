bc127dae3e52fdc52f00d5c83ee9b005e519f0a6
Merve <merve.tari@hotmail.com>
2022-06-20 17:34:59 +0300

    m

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,7 +6,20 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            // ya da böyle de yazılabilir.
+            Product product2 = new Product { Id = 2, CategoryId = 5, UnitsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+            ProductManager productManager = new ProductManager();
+
+
+
         }
     }
 }
