0e83ab3ac8867ec1ae53ba43de4cc879e7e62ce6
Merve <merve.tari@hotmail.com>
2022-06-25 18:02:00 +0300

    m

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items; 
+       public void Add(T item)
+       { 
+
+
+       }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Merve");
+
+            List<string> liste = new List<string>();
+            Console.WriteLine(liste.Count);  //count eleman sayısı demek.Yani listenin eleman sayısı
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
